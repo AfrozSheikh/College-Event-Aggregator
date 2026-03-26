864aea68151447ab79044ab8cfa96c78cb64946a
Nachiket858 <nachiketshinde2004@gmail.com>
2026-03-26 22:29:40 +0530

    HTML doc added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password', // EXACT wahi
-    database: 'college_event_system',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'college_event_system',
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
@@ -13,20 +13,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
-
-
-
-// const mysql = require('mysql2/promise');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST || 'localhost',
-//     user: process.env.DB_USER || 'root',
-//     password: process.env.DB_PASSWORD || 'password',
-//     database: process.env.DB_NAME || 'college_event_system',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
-// module.exports = pool;
